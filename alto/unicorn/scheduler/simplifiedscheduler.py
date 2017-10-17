88817b99658f65997251f1eed5f2d953189d6092
jensenzhang <hack@jensen-zhang.site>
2017-10-18 00:35:57 +0800

    Move filename input to a test method
    Signed-off-by: jensenzhang <hack@jensen-zhang.site>

--- alto/unicorn/scheduler/simplifiedscheduler.py
+++ alto/unicorn/scheduler/simplifiedscheduler.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
-from pulp import *
+from pulp import LpProblem, LpMaximize, LpVariable, \
+    LpStatus, lpSum
+
+def read_task_from_file(filename):
+    rsaCs = []
+    jobId2DataSize = {}
+    flow2JobId = {}
+    with open(filename) as f:
+        for line in f:
+            if str(line).__contains__("B"):
+                rsaCs.append(line)
+                flows = str(line).split(" ")[:-1]
+                for flow in flows:
+                    flow2JobId[flow] = str(flow).split("_")[1]
+            else:
+                jobId2DataSizeStr = str(line).split(" ")
+                for j2d in jobId2DataSizeStr:
+                    jobId = str(j2d).split("=")[0]
+                    dataSize = str(j2d).split("=")[1]
+                    jobId2DataSize[jobId] = dataSize
+    return rsaCs, jobId2DataSize, flow2JobId
 
 class SimpleScheduler:
     """
@@ -8,6 +28,7 @@ class SimpleScheduler:
     """
 
     def __init__(self):
+        # TODO: Make variables structured and human-readable
         self.bijkName2bijkVar = {}
         self.rsaCs = []
         self.jobId2DataSize = {}
@@ -17,6 +38,24 @@ class SimpleScheduler:
 
         self.prob = LpProblem("cms-problem", LpMaximize)
         self.prob += self.bsmall
+
+    def testFromFile(self, filename):
+        rsaCs, jobId2DataSize, flow2JobId = read_task_from_file(filename)
+        self.setGlobalVars(rsaCs, jobId2DataSize, flow2JobId)
+
+    def setGlobalVars(self, rsaCs, jobId2DataSize, flow2JobId):
+        self.rsaCs = rsaCs
+        self.jobId2DataSize = jobId2DataSize
+        for flow in flow2JobId:
+            jobId = str(flow).split("_")[1]
+            if jobId not in self.jobId2Num:
+                self.jobId2Num[jobId] = 1
+            else:
+                self.jobId2Num[jobId] += 1
+            if jobId not in self.jobId2Flows:
+                self.jobId2Flows[jobId] = [flow]
+            else:
+                self.jobId2Flows[jobId].append(flow)
 
     def setRSAConstraints(self):
         for rsaC in self.rsaCs:
@@ -33,37 +72,15 @@ class SimpleScheduler:
                 bijkL.append(bijk)
             self.prob += lpSum(bijkL) <= bw
 
-    def setGlobalVars(self, filename):
-        flow2JobId = {}
-        with open(filename) as f:
-            for line in f:
-                if str(line).__contains__("B"):
-                    self.rsaCs.append(line)
-                    flows = str(line).split(" ")[:-1]
-                    for flow in flows:
-                        flow2JobId[flow] = str(flow).split("_")[1]
-                else:
-                    jobId2DataSizeStr = str(line).split(" ")
-                    for j2d in jobId2DataSizeStr:
-                        jobId = str(j2d).split("=")[0]
-                        dataSize = str(j2d).split("=")[1]
-                        self.jobId2DataSize[jobId] = dataSize
-        for flow in flow2JobId:
-            jobId = str(flow).split("_")[1]
-            if jobId not in self.jobId2Num:
-                self.jobId2Num[jobId] = 1
-            else:
-                self.jobId2Num[jobId] += 1
-            if jobId not in self.jobId2Flows:
-                self.jobId2Flows[jobId] = [flow]
-            else:
-                self.jobId2Flows[jobId].append(flow)
-
     def setBijkAndBsmall(self):
         for jobId in self.jobId2DataSize:
             Bi = int(self.jobId2DataSize[jobId]) * self.bsmall
-            # self.prob += lpSum(self.bijkName2bijkVar[bijkName] for bijkName in self.jobId2Flows[jobId]) - Bi * self.jobId2Num[jobId] == 0
-            self.prob += lpSum(self.bijkName2bijkVar[bijkName] for bijkName in self.jobId2Flows[jobId]) - Bi == 0
+            self.prob += lpSum(
+                self.bijkName2bijkVar[bijkName]
+                for bijkName in self.jobId2Flows[jobId]) - Bi * self.jobId2Num[jobId] == 0
+            self.prob += lpSum(
+                self.bijkName2bijkVar[bijkName]
+                for bijkName in self.jobId2Flows[jobId]) - Bi == 0
 
     def report(self):
         print(LpStatus[self.prob.status])
@@ -83,12 +100,10 @@ class SimpleScheduler:
 if __name__ == '__main__':
     sched = SimpleScheduler()
 
-    sched.setGlobalVars("file1")
+    sched.testFromFile("file1")
     sched.setRSAConstraints()
     sched.setBijkAndBsmall()
 
     sched.info()
-
     sched.solve()
-
     sched.report()
