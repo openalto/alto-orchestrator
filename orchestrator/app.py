7dfa0e3f1d5de9eba805d131cfba5bf5a038193b
Y. Jace Liu <yang.jace.liu@linux.com>
2018-09-09 00:02:17 +0800

    Prepare for WEB UI
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -13,54 +13,62 @@ class DATA:
     FLOW_ID = 1
     id_map = {}
     sshd_servers = set()
+    flow_id_map = {}
 
 
 DomainData = {}
 
 
-class TasksEntry(object):
+class RunTaskEntry(object):
     def on_post(self, req, resp):
-        flows = json.loads(req.stream.read().decode("UTF-8"))
-        d = {
-            "query-desc": []
-        }
-        flow_id_map = {}
-        for flow in flows:
-            id = flow["id"]
-            jobs = flow["jobs"]
-            for job in jobs:
-                src_ip = job["potential_srcs"][0]["ip"]
-                dst_ip = job["potential_dsts"][0]["ip"]
-                dst_port = job["potential_dsts"][0]["port"]
-                d["query-desc"].append({
-                    "flow": {
-                        "flow-id": DATA.FLOW_ID,
-                        "src-ip": src_ip,
-                        "dst-ip": dst_ip,
-                        "dst-port": dst_port,
-                        "protocol": "tcp"
-                    }
-                })
-                flow_id_map[DATA.FLOW_ID] = {
-                    "flow": {
-                        "flow-id": DATA.FLOW_ID,
-                        "src-ip": src_ip,
-                        "dst-ip": dst_ip,
-                        "dst-port": dst_port,
-                        "protocol": "tcp"
-                    }
-                }
-                DATA.FLOW_ID += 1
-        print(DATA.RESOURCE_QUERY_URL)
-        print(d)
-        r = requests.post(DATA.RESOURCE_QUERY_URL, json=d, headers={"content_type": "application/json"})
-        r = json.loads(r.text)
-        print(r)
+        lp_results = json.loads(req.stream.read().decode('UTF-8'))
+
+        # Starting SSH server
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://127.0.0.1:12333")
+
+        for flow_result in lp_results["flows"]:
+            flow_id = flow_result["flow-id"]
+            bandwidth = flow_result["bandwidth"]
+            rate = int(bandwidth / 8.0) + 1
+
+            flow = DATA.flow_id_map[flow_id]
+            print(flow)
+
+            src_ip = flow['flow']["src-ip"]
+            dst_ip = flow['flow']["dst-ip"]
+            src_name = DATA.id_map[src_ip]
+            dst_name = DATA.id_map[dst_ip]
+
+            if dst_ip not in DATA.sshd_servers:
+                print("Starting SSH Server on %s", dst_ip)
+                socket.send_string("%s /usr/sbin/sshd -D &" % dst_name)
+                msg = socket.recv()
+                print(msg)
+
+                DATA.sshd_servers.add(dst_ip)
+
+            socket.send_string(
+                "%s ( dd if=/dev/zero bs=1M count=200 | pv --rate-limit %dk | ssh -oStrictHostKeyChecking=no %s dd of=/dev/null & )" % (
+                    src_name, rate, dst_ip
+                ))
+            msg = socket.recv()
+            print(msg)
+        socket.close()
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({"result": "OK"})
+
+
+class CalculateBandwidthEntry(object):
+    def on_post(self, req, resp):
+        r = json.loads(req.stream.read().decode('UTF-8'))
         ane_matrix = r["ane-matrix"]
-        flow_list = list()
+        flow_list = set()
         for row in ane_matrix:
             for item in row:
-                flow_list.append(int(item["flow-id"]))
+                flow_list.add(int(item["flow-id"]))
+        flow_list = sorted(flow_list)
         var_num = len(flow_list)
 
         # obj: min cx
@@ -81,44 +89,56 @@ class TasksEntry(object):
             A.append(A_r)
             b.append(60000)
 
-        # Starting SSH server
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://127.0.0.1:12333")
-
         res = linprog(c, A_ub=A, b_ub=b)
         print(res.x)
+
+        results = {"flows": []}
         for i, x in enumerate(res.x):
             flow_id = flow_list[i]
-            flow = flow_id_map[flow_id]
-            print(flow)
-            print(x)
-
-            src_ip = flow['flow']["src-ip"]
-            dst_ip = flow['flow']["dst-ip"]
-            src_name = DATA.id_map[src_ip]
-            dst_name = DATA.id_map[dst_ip]
-
-            if dst_ip not in DATA.sshd_servers:
-                print("Starting SSH Server on %s", dst_ip)
-                socket.send_string("%s /usr/sbin/sshd -D &" % dst_name)
-                msg = socket.recv()
-                print(msg)
-
-                DATA.sshd_servers.add(dst_ip)
-
-            rate = int(x / 8.0) + 1
-
-            socket.send_string(
-                "%s ( dd if=/dev/zero bs=1M count=200 | pv --rate-limit %dk | ssh -oStrictHostKeyChecking=no %s dd of=/dev/null & )" % (
-                    src_name, rate, dst_ip
-                ))
-            msg = socket.recv()
-            print(msg)
-        socket.close()
-
+            results["flows"].append({"flow-id": flow_id, "bandwidth": x})
         resp.status = falcon.HTTP_200
-        resp.body = """ {"code": "OK"} """
+        resp.body = json.dumps(results)
+
+
+class TasksEntry(object):
+    def on_post(self, req, resp):
+        flows = json.loads(req.stream.read().decode("UTF-8"))
+        d = {
+            "query-desc": []
+        }
+        DATA.flow_id_map = {}
+        for flow in flows:
+            id = flow["id"]
+            jobs = flow["jobs"]
+            for job in jobs:
+                src_ip = job["potential_srcs"][0]["ip"]
+                dst_ip = job["potential_dsts"][0]["ip"]
+                dst_port = job["potential_dsts"][0]["port"]
+                d["query-desc"].append({
+                    "flow": {
+                        "flow-id": DATA.FLOW_ID,
+                        "src-ip": src_ip,
+                        "dst-ip": dst_ip,
+                        "dst-port": dst_port,
+                        "protocol": "tcp"
+                    }
+                })
+                DATA.flow_id_map[DATA.FLOW_ID] = {
+                    "flow": {
+                        "flow-id": DATA.FLOW_ID,
+                        "src-ip": src_ip,
+                        "dst-ip": dst_ip,
+                        "dst-port": dst_port,
+                        "protocol": "tcp"
+                    }
+                }
+                DATA.FLOW_ID += 1
+        print(DATA.RESOURCE_QUERY_URL)
+        print(d)
+        r = requests.post(DATA.RESOURCE_QUERY_URL, json=d, headers={"content_type": "application/json"})
+        resp.status = falcon.HTTP_200
+        resp.body = r.text
+        print(r.text)
 
 
 class RegisterEntry(object):
@@ -138,5 +158,7 @@ app = falcon.API()
 
 app.add_route('/tasks', TasksEntry())
 app.add_route('/register', RegisterEntry())
+app.add_route('/calculate_bandwidth', CalculateBandwidthEntry())
+app.add_route('/run_task', RunTaskEntry())
 
 DATA.id_map = json.load(open('name-map.json'))
