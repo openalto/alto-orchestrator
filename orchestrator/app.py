1782e6474998f04acb916900c3d9d14f5133a5f7
Y. Jace Liu <yang.jace.liu@linux.com>
2018-09-10 10:16:05 +0800

    Add stop task entry
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- orchestrator/app.py
+++ orchestrator/app.py
@@ -154,11 +154,23 @@ class RegisterEntry(object):
         resp.body = """ {"code": "OK"} """
 
 
+class StopTaskEntry(object):
+    def on_post(self, req, resp):
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://127.0.0.1:12333")
+        host_name = list(DATA.id_map.values())[0]  # Choose a host name randomly
+        socket.send_string("%s pkill dd" % host_name)
+        socket.recv()
+        socket.close()
+
+
 app = falcon.API()
 
 app.add_route('/tasks', TasksEntry())
 app.add_route('/register', RegisterEntry())
 app.add_route('/calculate_bandwidth', CalculateBandwidthEntry())
 app.add_route('/run_task', RunTaskEntry())
+app.add_route('/stop_task', StopTaskEntry())
 
 DATA.id_map = json.load(open('name-map.json'))
